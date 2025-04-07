90aa1ad661c95632fe368964d5b91bf2d26f1050
Dedeke David Iteoluwakiisi <thatsaxydev@gmail.com>
2025-04-07 19:06:09 +0100

    added fade in for start button

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,15 +1,42 @@
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:tracer/notifiers/game_notifier.dart';
 import 'package:tracer/notifiers/ghost_input_notifier.dart';
+import 'package:tracer/shared/extensions.dart';
 import 'package:tracer/widgets/animated_sign.dart/animated_sign.dart';
 import 'race_view.dart';
 
-class HomeScreen extends ConsumerWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  final _showStart = false.notifier;
+
+  @override
+  void dispose() {
+    _showStart.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _showStartButton();
+  }
+
+  void _showStartButton() {
+    Future.delayed(3.seconds).then((_) {
+      _showStart.value = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Padding(
@@ -45,18 +72,37 @@ class HomeScreen extends ConsumerWidget {
                   ),
                 ],
               ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 32),
-                child: ElevatedButton(
-                  onPressed: () {
-                    ref.read(ghostInputProvider.notifier).clearGhostInput();
-                    ref.read(gameNotifierProvider.notifier).clearData();
-                    ref.read(ghostInputProvider.notifier).stopPlayback();
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (_) => const RaceScreen()));
-                  },
-                  child: const Text('Start'),
-                ),
+              _showStart.sync(
+                builder: (context, value, child) => _showStart.value
+                    ? Padding(
+                        padding: EdgeInsets.only(bottom: 32),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            ref
+                                .read(ghostInputProvider.notifier)
+                                .clearGhostInput();
+                            ref.read(gameNotifierProvider.notifier).clearData();
+                            ref
+                                .read(ghostInputProvider.notifier)
+                                .stopPlayback();
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (_) => const RaceScreen()));
+                          },
+                          child: const Text('Start'),
+                        ),
+                      ).fadeInFromBottom(delay: 0.ms)
+                    : Opacity(
+                        opacity: 0,
+                        child: Padding(
+                          padding: EdgeInsets.only(bottom: 32),
+                          child: ElevatedButton(
+                            onPressed: null,
+                            child: const Text('Start'),
+                          ),
+                        ),
+                      ),
               ),
             ],
           ),
