48caf67a47bb57f8619b12bf1d0e20cea5801241
Dedeke David Iteoluwakiisi <thatsaxydev@gmail.com>
2025-04-09 12:28:51 +0100

    adjusted home view UI

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -46,87 +46,97 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
         body: SizedBox(
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(32),
-              child: Stack(
-                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      TracerInsignia(
-                        height: 40,
-                        width: 200,
-                      ).fadeIn(delay: 400.ms),
-                      const SizedBox(height: 32),
-                      const Text(
-                        'T-Racer',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Courier',
-                        ),
-                      ).fadeIn(delay: 200.ms),
-                    ],
-                  ),
-                  _showStart.sync(
-                    builder: (context, value, child) => _showStart.value
-                        ? Padding(
-                            padding: EdgeInsets.only(bottom: 32),
-                            child: Column(
-                              spacing: 15,
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: List.generate(2, (index) {
-                                return Opacity(
-                                  opacity: index == 0 ? 1 : 0.3,
-                                  child: GameButton(
-                                          onPressed: () {
-                                            ref
-                                                .read(
-                                                    ghostInputProvider.notifier)
-                                                .clearGhostInput();
-                                            ref
-                                                .read(gameNotifierProvider
-                                                    .notifier)
-                                                .clearData();
-                                            ref
-                                                .read(
-                                                    ghostInputProvider.notifier)
-                                                .stopPlayback();
-
-                                            switch (index) {
-                                              case 0:
-                                                Navigator.push(
-                                                    context,
-                                                    MaterialPageRoute(
-                                                        builder: (_) =>
-                                                            const LevelSelectionView()));
-                                                break;
-                                              default:
-                                                ScaffoldMessenger.of(context)
-                                                    .showSnackBar(
-                                                        const SnackBar(
-                                                  content: Text(
-                                                      'Multiplayer mode is not available yet.'),
-                                                ));
-                                            }
-                                          },
-                                          text: switch (index) {
-                                            0 => 'VS Ghost',
-                                            _ => 'VS Player',
-                                          })
-                                      .fadeInFromBottom(
-                                          delay: (index * 400).ms),
-                                );
-                              }),
-                            ),
-                          )
-                        : const SizedBox.shrink(),
-                  ),
-                ],
+          child: Stack(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TracerInsignia(
+                      height: 40,
+                      width: 200,
+                    ).fadeIn(delay: 400.ms),
+                    const SizedBox(height: 32),
+                    const Text(
+                      'T-Racer',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Courier',
+                      ),
+                    ).fadeIn(delay: 200.ms),
+                  ],
+                ),
               ),
-            ),
+              _showStart.sync(
+                builder: (context, value, child) => _showStart.value
+                    ? Padding(
+                        padding: EdgeInsets.only(bottom: 100),
+                        child: SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          child: Column(
+                            spacing: 15,
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: List.generate(2, (index) {
+                              return Opacity(
+                                opacity: index == 0 ? 1 : 0.3,
+                                child: GameButton(
+                                        onPressed: () {
+                                          ref
+                                              .read(ghostInputProvider.notifier)
+                                              .clearGhostInput();
+                                          ref
+                                              .read(
+                                                  gameNotifierProvider.notifier)
+                                              .clearData();
+                                          ref
+                                              .read(ghostInputProvider.notifier)
+                                              .stopPlayback();
+
+                                          switch (index) {
+                                            case 0:
+                                              Navigator.push(
+                                                  context,
+                                                  MaterialPageRoute(
+                                                      builder: (_) =>
+                                                          const LevelSelectionView()));
+                                              break;
+                                            default:
+                                              ScaffoldMessenger.of(context)
+                                                  .showSnackBar(const SnackBar(
+                                                content: Text(
+                                                    'Multiplayer mode is not available yet.'),
+                                              ));
+                                          }
+                                        },
+                                        text: switch (index) {
+                                          0 => 'VS Ghost',
+                                          _ => 'VS Player',
+                                        })
+                                    .fadeInFromBottom(delay: (index * 400).ms),
+                              );
+                            }),
+                          ),
+                        ),
+                      )
+                    : const SizedBox.shrink(),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: 32),
+                  child: Text(
+                    'Made with 💙 by Kiishi',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w200,
+                      fontFamily: 'Courier',
+                      fontSize: 10,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
