9fc3b29bb004d8375f72962c1c34ac049d75764e
Dedeke David Iteoluwakiisi <thatsaxydev@gmail.com>
2025-04-09 12:48:09 +0100

    added web and mac os icons,and app display name for web

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Tracer',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: const Color(0xFF1E1E1E),
